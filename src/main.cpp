bed0e3173d5e29b817ac204473a4eb758374ac9c
BrunoWilkinson <bruno.wilkinson@gmail.com>
2022-09-30 13:02:37 +0100

    feat: class in use

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,24 @@
+#include <iostream>
+#include "deck.h"
+#include "card.h"
+
+void deal_hand(Deck & deck, int num_cards) {
+    while(num_cards--) {
+        Card card = deck.deal_card();
+        card.print();
+    }
+}
+
 int main()
 {
+    Deck deck;
+    std::cout << "there are " << deck.remaining() << " cards remaining" << std::endl;
+
+    deal_hand(deck, 9);
+    deal_hand(deck, 5);
+    deal_hand(deck, 13);
+    deal_hand(deck, 10);
+
+    std::cout << "there are " << deck.remaining() << " cards remaining" << std::endl;
     return 0;
 }
